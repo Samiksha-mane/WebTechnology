49448eb2c1b2d2b3060f012fa4a292b4c8f453eb
Samiksha Mane <samikshamane2005@gmail.com>
2026-01-14 19:39:35 +0530

    DataTypes

---
+++ DataTypes.js
@@ -0,0 +1,47 @@
+// String = A text of characters enclosed in quotes
+let color = "Red";
+let lastName = "Mane";
+console.log(color);
+console.log(lastName);
+
+// Number = A number representing a mathematical value
+let length = 16;
+console.log(length);
+let weight = 7.5;
+console.log(weight);
+
+// BigInt = A number representing a large integer
+let s = 1234567890123456789012345n;
+console.log(s);
+let p = BigInt(1234567890123456789012345)
+console.log(p);
+
+// Boolean = A data type representing true or false
+let a = true;
+let b = false;
+
+// Object = A collection of key-value pairs of data
+const person = {firstName:"Samikssha", lastName:"Mane"};
+console.log(person);
+
+// Array object
+const cars = ["Saab", "Volvo", "BMW"];
+console.log(cars);
+
+// Date object
+const date = new Date("2025-10-25");
+console.log(date);
+
+// Undefined = A primitive variable with no assigned value
+let q;
+let r;
+
+// Null = A primitive value representing object absence
+let t = null;
+let k = null;
+
+// Symbol = A unique and primitive identifier
+const x = Symbol();
+const y = Symbol();
+console.log(x);
+console.log(y);
