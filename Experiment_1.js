2a17da8838261aa998181160f1ec7b27d9aa91c4
Samiksha Mane <samikshamane2005@gmail.com>
2026-01-16 16:09:13 +0530

    Exp1

---
+++ Experiment_1.js
@@ -0,0 +1,59 @@
+// Activiy 1 = print student details
+document.writeln("<br> Activity 1<br>")
+let name = "Samiksha Bharat Mane";
+let id = 75;
+let email = "samikshamane2005@gmail.com";
+console.log("Student name : "+name);
+document.writeln("Student name : "+name);
+console.log("id :"+id);
+document.writeln("<br>id :"+id);
+console.log("email :"+email);
+document.writeln("<br>email :"+email);
+
+// activity 2 = Check even odd
+document.writeln("<br>Activity 2<br>")
+let x = 68
+if(x % 2 == 0)
+{
+    console.log("Even No :"+x);
+    document.writeln("Even No :"+x);
+}
+else
+{
+    console.log("<br>odd no :"+x);
+    document.writeln("<br>odd no :"+x)
+}
+
+//Activity 3 = check pass or fail
+document.writeln("<br>Activity 3") 
+let marks = 95
+if(marks > 35)
+{   console.log("Pass");
+    document.writeln("<br>Pass<br>");
+}
+else
+{
+    console.log("<br>Fail");
+    document.writeln("<br>Fail<br>");
+}
+
+// Activity 4 = display number
+document.writeln("Activity 4<br>")
+for(let i = 1;i<= 10; i ++)
+{
+    console.log(i);
+    document.writeln(i);
+}
+
+//Activity 5 
+document.writeln("<br>Activity 5<br>")
+let a = 100;
+let b = a;
+a = 123;
+console.log(a,b);
+document.writeln(a,b);
+// activity no 6
+
+
+    
+    
