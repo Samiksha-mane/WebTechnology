07348e3276e79a146e3e29196b5e21aace7f7894
Samiksha Mane <samikshamane2005@gmail.com>
2026-01-20 23:57:35 +0530

    DataTypes

--- DataTypes.js
+++ DataTypes.js
@@ -3,6 +3,25 @@ let color = "Red";
 let lastName = "Mane";
 console.log(color);
 console.log(lastName);
+let str = "ShoYO";
+console.log(str);
+console.log(typeof(str));
+
+//string interpoletion
+console.log(`Hello Dear ${str}`);
+let goruri = "Heyyyy";
+console.log(`${goruri}`);
+
+//use of new create = create a new object
+const g = new String("abc");
+console.log(g.length);
+
+console.log(g.toLowerCase());
+console.log(g.toUpperCase());
+
+//CharAT() = find char at position
+//INdexof() = find postion of the char 
+
 
 // Number = A number representing a mathematical value
 let length = 16;
@@ -20,13 +39,18 @@ console.log(p);
 let a = true;
 let b = false;
 
-// Object = A collection of key-value pairs of data
-const person = {firstName:"Samikssha", lastName:"Mane"};
+// Object = A collection of key-value pairs of data(non primitive)
+const person = {firstName:"Samiksha", lastName:"Mane"};
 console.log(person);
+console.log(person.firstName);
 
 // Array object
 const cars = ["Saab", "Volvo", "BMW"];
 console.log(cars);
+const Heroes = ['a','b','c'];
+console.log(Heroes);
+const num = [1,2,3,4];
+console.log(num);
 
 // Date object
 const date = new Date("2025-10-25");
@@ -40,8 +64,22 @@ let r;
 let t = null;
 let k = null;
 
-// Symbol = A unique and primitive identifier
+// Symbol = A unique and primitive identifier usend in react
 const x = Symbol();
 const y = Symbol();
+const anothrid = Symbol("123");
+console.log(anothrid);
+const id = 123;
+console.log(id === anothrid);
 console.log(x);
 console.log(y);
+
+//Function
+function name()
+{
+    console.log("Sam");
+    
+}
+console.log(name());
+
+
