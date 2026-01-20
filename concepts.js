04675028e0f7c4c2d62b19d228f3d351220424d0
Samiksha Mane <samikshamane2005@gmail.com>
2026-01-20 23:56:57 +0530

    Concepts

---
+++ concepts.js
@@ -0,0 +1,29 @@
+/* What is Web Technology?
+Web Technology refers to the tools, programming languages, 
+frameworks, and protocols used to create and run websites and 
+web applications on the internet.*/
+
+/* Real-time examples of Web Technology
+Online shopping websites = Amazon,filpcart,Mesho
+Social media apps = Instagram,facebook,whatsapp
+Online banking */
+
+/* Which file extension is used for JavaScript?
+The file extension used for JavaScript is .js.*/
+
+/* Why do we use Node.js?
+We use Node.js to run JavaScript on the server
+and build fast web applications.*/
+
+/*What is JavaScript
+JavaScript is the programming language of the web.
+It can calculate, manipulate and validate data.
+ It can update and change both HTML and CSS.*/
+
+ /* differnce between null and undefine
+    null = no any value
+  undefined = value is not assignet yet!
+ */
+// Javsscript is danymic
+
+//string,array,object,function are imp in js
