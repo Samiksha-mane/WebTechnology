f0a9761f385991ea4bfd64ff5e4eb62826acae12
Samiksha Mane <samikshamane2005@gmail.com>
2026-01-22 10:44:19 +0530

    Array_basic

---
+++ Array_basic.js
@@ -0,0 +1,36 @@
+//Array
+let myarr = [0,1,2,3,4,5];
+console.log(`My Array values ${myarr}`);
+console.log(typeof(myarr));
+
+const arr = new Array(0,1,2);//arrary decalration
+console.log(`My array :${arr}`);
+console.log(arr[1]);
+
+//Array Methods
+arr.push(7);//add values in array
+console.log(arr);
+
+arr.pop();//remove last value in array
+console.log(arr);
+
+arr.unshift(89);//it shifts all elements and add to 1st position
+console.log(arr);
+
+console.log(arr.shift());//removes the unshift element
+
+console.log(arr.includes(1));//check the element in the array
+
+console.log(arr.indexOf(2));//gives the element of that index
+
+//Activity = array slice and split
+//funtion
+//string
+//array
+
+
+
+
+
+
+
